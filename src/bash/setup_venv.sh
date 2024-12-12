ea32009daee600fa6450cef58b0e2420a804ccc6
Eduardo Azevedo <eduaze@gmail.com>
2024-12-12 15:54:21 -0500

    first commit

---
+++ src/bash/setup_venv.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Check if virtual environment directory exists
+if [ ! -d "venv" ]; then
+    # Create virtual environment
+    python3 -m venv venv
+    echo "Virtual environment created."
+else
+    echo "Virtual environment already exists."
+fi
+
+# Activate virtual environment
+source venv/bin/activate
+echo "Virtual environment activated."
+
+# Check if requirements.txt exists
+if [ -f "requirements.txt" ]; then
+    # Install requirements
+    pip install -r requirements.txt
+    echo "Requirements installed."
+else
+    echo "requirements.txt not found."
+fi
---
+++ src/py/test.py
@@ -0,0 +1,8 @@
+import os
+import config
+import sys
+
+print(os.getcwd())
+
+print(sys.path)
+print(config.OPENAI_API_KEY)
