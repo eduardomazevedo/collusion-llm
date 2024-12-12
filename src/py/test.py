04fa21fdfcebb37cd014e8a3179e6333840dc5cf
Eduardo Azevedo <eduaze@gmail.com>
2024-12-12 17:06:08 -0500

    install wrds

--- src/py/test.py
+++ src/py/test.py
@@ -6,3 +6,12 @@ print(os.getcwd())
 
 print(sys.path)
 print(config.OPENAI_API_KEY)
+
+
+import wrds
+print (wrds.Connection)
+
+conn = wrds.Connection(wrds_username=config.WRDS_USERNAME, password=config.WRDS_PASSWORD)
+print(conn)
+
+print(conn.list_libraries())
