321df5c380cad399e3efbb3cbf0d2dfde1a108bb
Eduardo Azevedo <eduaze@gmail.com>
2024-12-12 22:10:13 -0500

    run notebooks sh

--- src/bash/download_credentials.sh
+++ src/bash/download_credentials.sh
@@ -3,3 +3,11 @@
 rclone copyto collusion-llm:/credentials/env-file ./.env
 
 echo "Downloaded credentials from rclone remote to ./.env"
+
+# Add ROOT variable with path for the root to .env:
+PROJECT_ROOT=$(git rev-parse --show-toplevel)
+echo "" >> .env
+echo "# Root folder" >> .env
+echo "ROOT=$PROJECT_ROOT" >> ./.env
+
+echo "Added ROOT variable with path for the root to .env"
